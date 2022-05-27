f35a4f6457e9727e7d28af11571643234ec5146b
unknown <wcaio41@gmail.com>
2022-05-27 13:46:14 -0300

    update login, logout and register

--- Tasks/views.py
+++ Tasks/views.py
@@ -1,11 +1,13 @@
 from datetime import date
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task, User
+from django.contrib.auth.decorators import login_required
 
+@login_required(redirect_field_name='login')
 def index(request):
     tasks = Task.objects.filter(user_id=request.user.id).order_by('-id')
     return render(request, 'pages/index.html', {'tasks':tasks})
-
+@login_required(redirect_field_name='login')
 def add_task(request):
     if request.method == 'POST':
         task = request.POST.get('task')
@@ -16,19 +18,19 @@ def add_task(request):
     else:
         return render(request, 'pages/add_task.html')
 
-
+@login_required(redirect_field_name='login')
 def finish_task(request, id):
     task = get_object_or_404(Task, id=id)
     task.status = True
     task.save()
     return redirect('home')
 
-
+@login_required(redirect_field_name='login')
 def delete_task(request, id):
     task = get_object_or_404(Task, id=id)
     task.delete()
     return redirect('home')
-
+@login_required(redirect_field_name='login')
 def edit_task(request, id):
     
     if request.method == 'POST':
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
+from django.contrib.auth.models import User
 
 def user_login(request):
     if request.method == 'POST':
@@ -8,9 +9,25 @@ def user_login(request):
         check_user = auth.authenticate(username=user, password=password)
         print(check_user)
         if check_user is not None:
+            auth.login(request, check_user)
             return redirect('home')
         else:
             return render(request, 'pages/login.html')
 
     else:
         return render(request, 'pages/login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
+
+#EFETUAR VALIDAÇÕES
+def register(request):
+    if request.method == 'POST':
+        user = request.POST.get('user')
+        password = request.POST.get('pass')
+        password_2 = request.POST.get('pass_2')
+        create = User.objects.create_user(username=user, password=password)
+        return redirect('login')
+    else:    
+        return render(request, 'pages/register.html')
